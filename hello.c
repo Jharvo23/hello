c42f18b79abc7a43ba146bb3c93a19f5ddb00bc0
Jackson Harvey <jharvey34@huskers.unl.edu>
2024-08-28 00:14:14 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ */
+
+int main(int argc, char **argv) {
+
+    printf("Hello, I'm Jackson Harvey!\n");
+    printf("Major is Electrical Engineering!\n");
+
+    return0;
+}
