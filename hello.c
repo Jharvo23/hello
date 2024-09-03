95b8f637001ed010620563affee1613c64a121a8
Jharvo23 <jharvey34@huskers.unl.edu>
2024-09-03 00:51:46 -0500

    fixed standard library bug

--- hello.c
+++ hello.c
@@ -1,11 +1,13 @@
 /**
  * Basic Hello World Program
  */
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
 
     printf("Hello, I'm Jackson Harvey!\n");
     printf("Major is Electrical Engineering!\n");
 
-    return0;
+    return 0;
 }
